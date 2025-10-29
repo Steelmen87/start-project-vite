465049f46ba0dc9594a62358277fb280e8ffe69b
Steel <Steelmen87@bk.ru>
2025-10-29 17:02:15 +0300

    Skills +string = any

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import {HeaderMenu} from "./layout/header/Header.tsx";
 import {Container} from "./layout/container/Container.tsx";
 import {Main} from "./layout/sections/main/Main.tsx";
+import {AboutMe} from "./layout/sections/aboutMe/AboutMe.tsx";
+import {Skills} from "./layout/sections/skills/Skills.tsx";
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
         <Container>
             <HeaderMenu/>
             <Main/>
+            <AboutMe/>
+            <Skills/>
         </Container>
     )
 }
--- src/layout/container/Container.tsx
+++ src/layout/container/Container.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
+import { StyledMe} from "../sections/aboutMe/AboutMe.tsx";
 
 export const Container = styled.div`
     display: flex;
     align-items: center;
     flex-direction: column;
+    
+    ${StyledMe} {
+        background: rgba(246, 246, 246, 1);
+
+    }
 `
